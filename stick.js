54b04986e5b89294940157193726f01b508762c5
dldyou <chan12@konkuk.ac.kr>
2023-05-12 21:36:08 +0900

    test : 막대 추가

---
+++ stick.js
@@ -0,0 +1,11 @@
+export class Stick {
+    constructor(x, y, width, height, speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    
+}
