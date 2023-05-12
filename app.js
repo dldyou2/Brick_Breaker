efdb4e6e0fc1a1f9ff39e9ac6df8fe4fabf8c474
dldyou <chan12@konkuk.ac.kr>
2023-05-12 17:42:02 +0900

    test: standard code

---
+++ app.js
@@ -0,0 +1,98 @@
+import { Ball } from "./ball.js";
+var canvas, ctx; // 캔버스와 컨텍스트
+/*
+Author : 윤찬규
+Date : 2023-05-12
+Description: 초기화 함수
+
+맨 처음 한 번 실행시키는 함수입니다.
+*/
+function Init() {
+    CanvasInit();
+
+    // test call
+    console.log("initialize complete");
+}
+
+/*
+Author : 윤찬규
+Date : 2023-05-12
+Description: 캔버스 초기화 함수
+
+canvas와 ctx를 초기화 후 ctx에서 그리기를 시작합니다.
+*/
+function CanvasInit() {
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+
+    // test call
+    console.log("canvas initialize complete");
+}
+
+/*
+Author : 윤찬규
+Date : 2023-05-12
+Description : 인게임 함수
+
+게임 플레이의 전제를 관장합니다.
+*/
+function InGame(difficulty) {
+    InGameInit(difficulty);
+    GamePlay();
+
+    // test call
+    console.log("ingame complete")
+}
+
+/*
+Author : 윤찬규
+Date : 2023-05-12
+Description : 인게임 초기화 함수
+
+게임 시작에 필요한 것들을 초기화하는 함수입니다.
+*/
+function InGameInit(difficulty) {
+    if(difficulty == 1) {
+
+    }
+    else if(difficulty == 2) {
+
+    }
+    else if(difficulty == 3) {
+
+    }
+
+    // test call
+    console.log("game difficulty is " + difficulty);
+}
+/*
+Author : 윤찬규
+Date : 2023-05-12
+Description : 실제 게임이 작동되는 함수
+*/
+function GamePlay() {
+    let ball = new Ball(150, 300, 15, 30, 5);
+    let interval;
+    function move() {
+        ball.move(ctx);
+        if(ball.isLeft()) {
+            clearInterval(interval);
+
+            // test call
+            console.log("ball is out")
+        }
+        // test call
+        // console.log("Ball pos [" + ball.x + "," + ball.y + "] ang: " + ball.angle + " size: " + ball.r);
+    }
+    
+    interval = setInterval(move, 10);
+    
+    // test call
+    console.log("game is playing now");
+}
+
+window.onload = () => {
+    Init();
+    // 난이도를 선택하면 아래 함수를 실행합니다.
+    InGame(1); 
+}
---
+++ ball.js
@@ -0,0 +1,76 @@
+/*
+Author: 윤찬규
+Data: 2023-05-12
+Description: 공 클래스
+
+공의 위치 / 크기 / 이동 방향 / 속도 등을 저장 가능합니다.
+*/
+export class Ball {
+    constructor(x, y, r, angle, speed) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.angle = angle;
+        this.speed = speed;
+        this.color = "red";
+    }
+
+    /*
+    Author : 윤찬규
+    Data : 2023-05-12
+    Description : 도 -> 라디안 변환 함수
+
+    private 함수입니다. 멤버에서만 접근 가능
+    */
+    #d2r(angle) {
+        return Math.PI * angle / 180;
+    }
+
+    /*
+    Author : 윤찬규
+    Data : 2023-05-12
+    Description : 공을 움직이는 함수입니다.
+
+    왼쪽 벽을 제외한 나머지 벽들에 대한 반사와 각 / 속도에 따른 좌표 변화를 적용합니다.
+    */
+    move(ctx) {
+        ctx.clearRect(this.x - this.r - 1, this.y - this.r - 1, this.r * 2 + 2, this.r * 2 + 2);
+        if(this.x + this.r >= 1400) {
+            this.angle = (540 - this.angle) % 360; 
+        } 
+        else if(this.y - this.r <= 0 || 
+            this.y + this.r >= 600) {
+            this.angle = 360 - this.angle;
+        } 
+        let radian = this.#d2r(this.angle);
+        this.x += Math.cos(radian) * this.speed;
+        this.y += Math.sin(radian) * this.speed; 
+        this.draw(ctx);
+    }
+
+    /*
+    Author : 윤찬규 
+    Data : 2023-05-12
+    Description : 공을 그리는 함수입니다.
+
+    context를 인자로 받아와서 해당 context에 그립니다.
+    */
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(Math.floor(this.x), Math.floor(this.y), Math.floor(this.r), 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    /*
+    Author : 윤찬규
+    Data : 2023-05-12
+    Description : 공이 왼쪽 벽에 닿았는지 확인하는 함수입니다.
+
+    닿으면 1 아니면 0을 리턴합니다. 벽의 기준은 해당 좌표값 왼쪽입니다.
+    */
+    isLeft() {
+        return this.x <= 100;
+    }
+}
